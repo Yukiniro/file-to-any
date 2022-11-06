60b792f2667693df0d26a003f96ba53238983254
Yukiniro <yukiniro@hotmail.com>
2022-11-06 16:49:28 +0800

    chore: remove unused test

--- __test__/hello.test.ts
+++ __test__/hello.test.ts
@@ -1,6 +1,3 @@
 import { test, expect } from "vitest";
-import { sayHello } from "../src";
 
-test("hello world", () => {
-  expect(sayHello()).toBe("Hello World");
-});
+test("hello world", () => {});
