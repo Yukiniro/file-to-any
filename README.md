da8d05d6a187c208f7d99232a76f874c031425f5
Yukiniro <yukiniro@hotmail.com>
2022-11-06 15:02:09 +0800

    feat: init

---
+++ README.md
@@ -0,0 +1,21 @@
+# ssprojet
+
+![NPM](https://img.shields.io/npm/l/ssprojet?color=blue&style=flat-square) ![npm](https://img.shields.io/npm/v/ssprojet?color=blue&style=flat-square)
+
+The `ssprojet` is a template to start a simple project.
+
+## Features
+
+- `pnpm` - package manager
+- `vitest` - test library
+- `docusaurus` - document library
+- `prettier` - code style formate
+- `eslint` - code quality
+- `husky` - pre-commit
+- `lint-staged` - auto lint for staged files
+
+## How to use
+
+You could use `degit Yukiniro/ssprojet your-project-name` to create a project.
+
+If you have no `degit`. Please install it by `npm install -g degit`.
---
+++ rollup.config.mjs
@@ -0,0 +1,42 @@
+import { babel } from "@rollup/plugin-babel";
+import commonjs from "@rollup/plugin-commonjs";
+import { nodeResolve } from "@rollup/plugin-node-resolve";
+import fileSize from "rollup-plugin-filesize";
+import typescript from "rollup-plugin-typescript2";
+import { readFile } from "fs/promises";
+
+function getConfig(libraryName) {
+  return {
+    input: "src/index.ts",
+    output: [
+      {
+        file: `./dist/${libraryName}.cjs`,
+        format: "cjs",
+      },
+      {
+        file: `./dist/${libraryName}.mjs`,
+        format: "esm",
+      },
+    ],
+    plugins: [
+      commonjs(),
+      nodeResolve(),
+      typescript(),
+      babel({
+        babelHelpers: "bundled",
+        presets: ["@babel/preset-env"],
+      }),
+      fileSize(),
+    ],
+  };
+}
+
+export default readFile("./package.json", "utf8")
+  .then((json) => {
+    const libraryName = JSON.parse(json).name;
+    return getConfig(libraryName);
+  })
+  .catch((e) => {
+    console.log(e);
+    return getConfig("my-project");
+  });
