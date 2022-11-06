84947fcc95cd9d95c2d6a4b3b873337b4a7a0aac
Yukiniro <yukiniro@hotmail.com>
2022-11-06 16:48:25 +0800

    feat: create toArrayBuffer and toAny

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,33 @@
-export const sayHello = function () {
-  return "Hello World";
-};
+import { isString, isUndefined } from "bittydash";
+
+export type TargetFile = File | Blob;
+export type TypeOption = "arrayBuffer";
+export type ObjOption = { type: TypeOption };
+
+export type Options = TypeOption | ObjOption;
+
+async function toArrayBuffer(file: TargetFile) {
+  await new Promise((resolve, reject) => {
+    const fileReader = new FileReader();
+    fileReader.onload = resolve;
+    fileReader.onerror = reject;
+    fileReader.onabort = reject;
+    fileReader.readAsArrayBuffer(file);
+  });
+}
+
+async function toAny(file: TargetFile, options?: Options) {
+  const type = isUndefined(options)
+    ? "arrayBuffer"
+    : isString(options)
+    ? options
+    : (options as ObjOption).type;
+
+  if (type === "arrayBuffer") {
+    return toArrayBuffer(file);
+  }
+
+  throw new Error("Convert type is invalid");
+}
+
+export { toAny };
