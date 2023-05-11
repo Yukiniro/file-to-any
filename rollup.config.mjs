a74fdacfc0990bf89d4f9209740096b964c3498a
Yukiniro <yukiniro@hotmail.com>
2023-05-11 22:06:12 +0800

    chore: rewrite rollup.config.mjs

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -5,38 +5,29 @@ import fileSize from "rollup-plugin-filesize";
 import typescript from "rollup-plugin-typescript2";
 import { readFile } from "fs/promises";
 
-function getConfig(libraryName) {
-  return {
-    input: "src/index.ts",
-    output: [
-      {
-        file: `./dist/${libraryName}.cjs`,
-        format: "cjs",
-      },
-      {
-        file: `./dist/${libraryName}.mjs`,
-        format: "esm",
-      },
-    ],
-    plugins: [
-      commonjs(),
-      nodeResolve(),
-      typescript(),
-      babel({
-        babelHelpers: "bundled",
-        presets: ["@babel/preset-env"],
-      }),
-      fileSize(),
-    ],
-  };
-}
+const json = await readFile("./package.json", "utf8");
+const libraryName = JSON.parse(json).name || "my-project";
 
-export default readFile("./package.json", "utf8")
-  .then((json) => {
-    const libraryName = JSON.parse(json).name;
-    return getConfig(libraryName);
-  })
-  .catch((e) => {
-    console.log(e);
-    return getConfig("my-project");
-  });
+export default {
+  input: "src/index.ts",
+  output: [
+    {
+      file: `./dist/${libraryName}.cjs`,
+      format: "cjs",
+    },
+    {
+      file: `./dist/${libraryName}.mjs`,
+      format: "esm",
+    },
+  ],
+  plugins: [
+    commonjs(),
+    nodeResolve(),
+    typescript(),
+    babel({
+      babelHelpers: "bundled",
+      presets: ["@babel/preset-env"],
+    }),
+    fileSize(),
+  ],
+};
